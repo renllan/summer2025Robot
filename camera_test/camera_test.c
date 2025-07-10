b4566414238af186becfbff96a2cd9f4c2f98d57
Ren Liang Lan <brighlan5288@gmail.com>
2025-07-10 16:48:37 -0400

    added saving

--- camera_test/camera_test.c
+++ camera_test/camera_test.c
@@ -1,4 +1,22 @@
 #include "../asbae_robot/hw6.h"
+#include <stdio.h>
+
+// Save image as PPM (P6, binary)
+void save_rgb_image_as_ppm(const char* filename, struct pixel_format_RGB* img_data, int width, int height) {
+    FILE* f = fopen(filename, "wb");
+    if (!f) {
+        printf("Failed to open %s for writing\n", filename);
+        return;
+    }
+    fprintf(f, "P6\n%d %d\n255\n", width, height);
+    for (int i = 0; i < width * height; i++) {
+        fputc(img_data[i].R, f);
+        fputc(img_data[i].G, f);
+        fputc(img_data[i].B, f);
+    }
+    fclose(f);
+    printf("Saved %s\n", filename);
+}
 
 int main(int argc, char * argv[]){
     struct video_interface_handle_t *         handle_video1;
@@ -9,36 +27,70 @@ int main(int argc, char * argv[]){
 
     unsigned char               IMG_RAW1[IMAGE_SIZE];//arm image
     struct pixel_format_RGB     * IMG_DATA1;
-    unsigned char               IMG_RAW[IMAGE_SIZE];
-    struct pixel_format_RGB     * IMG_DATA;
+    unsigned char               IMG_RAW0[IMAGE_SIZE];
+    struct pixel_format_RGB     * IMG_DATA0;
+    unsigned char               IMG_RAW2[IMAGE_SIZE];//arm image
+    struct pixel_format_RGB     * IMG_DATA2;
+    unsigned char               IMG_RAW3[IMAGE_SIZE];
+    struct pixel_format_RGB     * IMG_DATA3;
 
     IMG_DATA1 = (struct pixel_format_RGB*)&IMG_RAW1;
-    IMG_DATA = (struct pixel_format_RGB*)&IMG_RAW;
+    IMG_DATA0 = (struct pixel_format_RGB*)&IMG_RAW0;
 
-
+    IMG_DATA2 = (struct pixel_format_RGB*)&IMG_RAW2;
+    IMG_DATA3 = (struct pixel_format_RGB*)&IMG_RAW3;
 
     handle_video1 = video_interface_open( "/dev/video0" );
     video_interface_print_modes(handle_video1);
 
-    if(!video_interface_set_mode_manual(handle_video1,3)){
+    if(!video_interface_set_mode_manual(handle_video1,7)){
         printf("failed to configure dev/video0 \n");
         return 1;
     }
     
 
 
-    handle_video2 = video_interface_open( "/dev/video2" );
-    video_interface_print_modes(handle_video1);
 
-    if(!video_interface_set_mode_manual(handle_video1,3)){
+
+    handle_video2 = video_interface_open( "/dev/video2" );
+    video_interface_print_modes(handle_video2);
+
+    if(!video_interface_set_mode_manual(handle_video2,7)){
         printf("failed to configure dev/video0 \n");
         return 1;
     }
 
     if(handle_video1 && handle_video2)
     {
+        for(int  i = 0 ; i<4;i++)
+        {
+
+        }
         video_interface_get_image(handle_video1,&image);
+        memcpy(IMG_RAW0, (unsigned char *)&image,IMAGE_SIZE);
+        memcpy(IMG_RAW1, (unsigned char *)&image,IMAGE_SIZE);
+        memcpy(IMG_RAW2, (unsigned char *)&image,IMAGE_SIZE);
+        memcpy(IMG_RAW3, (unsigned char *)&image,IMAGE_SIZE);
+
+        save_rgb_image_as_ppm("cam1_cp1",IMG_DATA1,IMG_WIDTH,IMG_HEIGHT);
+        save_rgb_image_as_ppm("cam1_cp2",IMG_DATA2,IMG_WIDTH,IMG_HEIGHT);
+        save_rgb_image_as_ppm("cam1_cp3",IMG_DATA3,IMG_WIDTH,IMG_HEIGHT);
+        save_rgb_image_as_ppm("cam1_cp4",IMG_DATA0,IMG_WIDTH,IMG_HEIGHT);
+
         video_interface_get_image(handle_video2,&image1);
+        memcpy(IMG_RAW1, (unsigned char*)&image1, IMAGE_SIZE);
+        memcpy(IMG_RAW0, (unsigned char *)&image1,IMAGE_SIZE);
+        memcpy(IMG_RAW2, (unsigned char *)&image1,IMAGE_SIZE);
+        memcpy(IMG_RAW3, (unsigned char *)&image1,IMAGE_SIZE);
+
+        save_rgb_image_as_ppm("cam2_cp1",IMG_DATA1,IMG_WIDTH,IMG_HEIGHT);
+        save_rgb_image_as_ppm("cam2_cp2",IMG_DATA2,IMG_WIDTH,IMG_HEIGHT);
+        save_rgb_image_as_ppm("cam2_cp3",IMG_DATA3,IMG_WIDTH,IMG_HEIGHT);
+        save_rgb_image_as_ppm("cam2_cp4",IMG_DATA0,IMG_WIDTH,IMG_HEIGHT);
+
+        
+
+
     }
 }
 
