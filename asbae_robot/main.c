b440d12f5e918111bd772ec146aa9c6524efe090
Ren Liang Lan <brighlan5288@gmail.com>
2025-07-10 11:31:52 -0400

    skidbid

--- asbae_robot/main.c
+++ asbae_robot/main.c
@@ -388,7 +388,7 @@ int main(int argc, char * argv[] )
      * For now, CLAW_OPEN is set to 500 and CLAW_CLOSE is set to 800
      */
     printf("Setting claw to CLAW_OPEN\n\n");
-    int claw_pos = CLAW_OPEN;
+    claw_pos = CLAW_OPEN;
     set_claw(uart_fd, claw_pos, ARM_CLAW_TIMEOUT);
 
     /*
