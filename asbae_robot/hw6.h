de8559ffbcc6ad5966bb19b5ce926ae7d2888b61
Ren Liang Lan <brighlan5288@gmail.com>
2025-07-10 20:02:38 -0400

    modified make file

--- asbae_robot/hw6.h
+++ asbae_robot/hw6.h
@@ -31,7 +31,7 @@
 #include "../include/MPU9250.h"
 #include "../include/wait_key.h"
 #include "pixel_format_RGB.h"
-#include "video_interface.h"
+#include "../include/video_interface.h"
 #include "wait_key.h"
 #include "scale_image_data.h"
 #include "draw_bitmap_multiwindow.h"
