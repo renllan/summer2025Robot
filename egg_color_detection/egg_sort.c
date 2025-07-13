2d95b453df343ffc045f49a54bc1765a056b78eb
Ren Liang Lan <brighlan5288@gmail.com>
2025-07-13 12:10:07 -0400

    change camera input

--- egg_color_detection/egg_sort.c
+++ egg_color_detection/egg_sort.c
@@ -33,7 +33,7 @@ int egg_sort(int center_x, int center_y, struct pixel_format_RGB *img)
         int avg_r = sum_r / count;
         int avg_g = sum_g / count;
         int avg_b = sum_b / count;
-        //printf("Average RGB inside bbox: (R: %d, G:%d, B:%d)\n", avg_r, avg_g, avg_b);
+        printf("Average RGB inside bbox: (R: %d, G:%d, B:%d)\n", avg_r, avg_g, avg_b);
     //return the avg blue pixel value in the small box
         return avg_b;
     }
@@ -67,7 +67,7 @@ int main(int argc, char * argv[])
     struct draw_bitmap_multiwindow_handle_t * handle_GUI_grey 
     =  draw_bitmap_create_window(IMG_WIDTH,IMG_HEIGHT);
 
-    handle_video1 = video_interface_open("/dev/video0");
+    handle_video1 = video_interface_open("/dev/video2");
     
     if(!video_interface_set_mode_auto(handle_video1)){
         printf("failed to configure dev/video0 \n");
