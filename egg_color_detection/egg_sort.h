6b17ed4553f0b62b77fb0037f95c2785863ee6b8
ey <ey@le-Mac.local>
2025-07-13 12:17:46 -0400

    added arm lib

--- egg_color_detection/egg_sort.h
+++ egg_color_detection/egg_sort.h
@@ -1,6 +1,7 @@
 #include "../include/pixel_format_RGB.h"
 #include "../asbae_robot/hw6.h"
 #include "../asbae_robot/draw_bbox.h"
+#include "../asbae_robot/C_equiv_ctl.h"
 
 #define IMG_WIDTH 320
 #define IMG_HEIGHT 240
