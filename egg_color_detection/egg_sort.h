cf3adf954b9a783372633371cc092e743901a831
Ren Liang Lan <brighlan5288@gmail.com>
2025-07-13 12:07:01 -0400

    made led work

--- egg_color_detection/egg_sort.h
+++ egg_color_detection/egg_sort.h
@@ -4,6 +4,6 @@
 
 #define IMG_WIDTH 320
 #define IMG_HEIGHT 240
-#define BBOX_W   8
-#define BBOX_H   8
+#define BBOX_W   16
+#define BBOX_H   16
 int egg_sort(int center_x, int center_y, struct pixel_format_RGB *img);
