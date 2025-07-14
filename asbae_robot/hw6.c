0e9563de0e31ad3e99c5dc3290d7dabc3617e562
Ren Liang Lan <brighlan5288@gmail.com>
2025-07-14 13:48:00 -0400

    ir sensor

--- asbae_robot/hw6.c
+++ asbae_robot/hw6.c
@@ -95,8 +95,8 @@ void *IR_Sensor(void* arg)
 
 
       if(!pause_thread){ 
-        int left_val = GPIO_READ(param->gpio,param->pin_1); // read ground
-        int right_val = GPIO_READ(param->gpio,param->pin_2 ); //read wall
+        int left_val = GPIO_READ(param->gpio,param->pin_2); // read ground
+        int right_val = GPIO_READ(param->gpio,param->pin_1 ); //read wall
         
       if(left_val == 1){ 
 
