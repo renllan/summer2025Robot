6a6c0eb72faf1ae59a6892ba9a072388909a34ad
Ren Liang Lan <brighlan5288@gmail.com>
2025-07-14 10:24:24 -0400

    increase busy count

--- asbae_robot/hw6.c
+++ asbae_robot/hw6.c
@@ -2101,7 +2101,7 @@ void *egg_detector(void * arg){
                 if(robot_centered && robot_stopped){
                   printf("robot is close enough to grab the egg \n");
 
-                  for(int i = 0; i< 50;i++){
+                  for(int i = 0; i< 75;i++){
                     wait_period(&timer_state, 10u);
                   }
                   if(!FIFO_FULL(param->dir_fifo)){
