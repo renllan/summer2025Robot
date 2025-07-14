4141dc5396d1fbccb8d0b845cefd2f03afee7e80
Ren Liang Lan <brighlan5288@gmail.com>
2025-07-13 21:53:23 -0400

    fixed claw macro

--- asbae_robot/hw6.h
+++ asbae_robot/hw6.h
@@ -89,8 +89,8 @@
 #define UPDOWN_MOTOR_L 0 
 #define UPDOWN_MOTOR_R 180
 
-#define CLAW_OPEN 400 // 500 for other car
-#define CLAW_CLOSE 850 // 800 for other car
+#define CLAW_OPEN 325 // 500 for other car
+#define CLAW_CLOSE 875 // 800 for other car
 #define ARM_TIMEOUT 500 // Timeout for arm commands in milliseconds
 #define PWM_SERVO_TIMEOUT 375 // Timeout for pwm servo commands in milliseconds
 #define ARM_CLAW_TIMEOUT 500 // Timeout for arm claw commands in milliseconds
